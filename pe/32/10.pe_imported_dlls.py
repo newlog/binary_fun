8045954bce5024f2c9c1af2e244a7b8494d114c1
Albert Lopez <albert.lopez@attackiq.com>
2017-12-10 12:14:12 -0800

    Add code to parse imported dll names

---
+++ pe/32/10.pe_imported_dlls.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+import struct
+import logging
+import sys
+from libs.pe_section_utils import PESectionUtils
+
+
+class FileUtils(object):
+
+    @staticmethod
+    def read_file(path):
+        file_contents = None
+        try:
+            with open(path, 'rb') as fd:
+                file_contents = fd.read()
+        except IOError as e:
+            logging.error('Error reading file "{}". Error: {}'.format(path, e))
+        return file_contents
+
+
+class ParsePE32(object):
+
+    def __init__(self):
+        self.bin_contents = None
+        self.image_dos_header_offs = 0
+        self.pe_header_offs = None  # needs to be computed dynamically
+        self.file_header_offs = None  # needs to be computed dynamically
+        self.optional_header_offs = None  # needs to be computed dynamically
+        self.number_of_sections = None  # needs to be computed dynamically
+        self.section_headers_info = None  # needs to be computed dynamically
+        self.pe_section_utils = None
+
+    def execute(self, bin_path):
+        self.bin_contents = FileUtils.read_file(bin_path)
+        if self.bin_contents:
+            self.parse_pe32()
+
+    def parse_pe32(self):
+        self.pe_section_utils = PESectionUtils(self.bin_contents)
+        self.section_headers_info = self.pe_section_utils.get_sections_info()
+        imported_dlls = self.get_imported_dlls()
+        logging.info('PE Imported DLLs #: {}'.format(len(imported_dlls)))
+        logging.info('PE Imported DLLs: \n- {}'.format('\n- '.join(imported_dlls)))
+
+    def get_imported_dlls(self):
+        dll_names = []
+        current_import_descriptor = self.get_first_import_descriptor_foff()
+        import_descriptor_size = 0x14  # self.get_import_descriptor_size()
+        while True:
+            name = self.get_dll_name(current_import_descriptor)
+            if not name:
+                break
+            dll_names.append(name)
+            current_import_descriptor += import_descriptor_size
+        return dll_names
+
+    def get_data_directory_array_offs(self):  # _IMAGE_DATA_DIRECTORY DataDirectory[16]; from OptionalHeader
+        optional_header_offs = self.pe_section_utils.get_optional_header_offs()
+        data_directories_offs = optional_header_offs + 0x60
+        return data_directories_offs
+
+    def get_import_directory_offs(self):  # IMAGE_DIRECTORY_ENTRY_IMPORT (DataDirectory[1])
+        data_directory_array_offs = self.get_data_directory_array_offs()
+        image_data_directory_size = 0x8  # two dwords
+        return data_directory_array_offs + image_data_directory_size * 1
+
+    def get_import_descriptor_size(self):
+        import_directory_offs = self.get_import_directory_offs()
+        size_offs = 0x4
+        # format string: <: little endian. H: 2 bytes (1 word)
+        import_descriptor_size = struct.unpack_from('<H', self.bin_contents, import_directory_offs + size_offs)
+        return import_descriptor_size[0]
+
+    def get_first_import_descriptor_rva(self):
+        import_directory_offs = self.get_import_directory_offs()
+        virtual_address_offs = 0x0
+        # format string: <: little endian. H: 2 bytes (1 word)
+        import_descriptor_rva = struct.unpack_from('<H', self.bin_contents, import_directory_offs + virtual_address_offs)
+        return import_descriptor_rva[0]
+
+    def get_first_import_descriptor_foff(self):
+        import_directory_offs = self.get_first_import_descriptor_rva()
+        return self._rva_to_file_offset(import_directory_offs)
+
+    def get_dll_name(self, import_descriptor_addr):
+        name_offs = 0xC
+        # format string: <: little endian. L: 4 bytes (1 dword)
+        name_rva = struct.unpack_from('<L', self.bin_contents, import_descriptor_addr + name_offs)
+        name_foff = self._rva_to_file_offset(name_rva[0])
+        name = self._read_null_terminated_ascii_string(name_foff)
+        return name
+
+    def _read_null_terminated_ascii_string(self, starting_addr):
+        idx = 0
+        ascii_string = ''
+        while ord(self.bin_contents[starting_addr + idx]) != 0 and starting_addr + idx < len(self.bin_contents):
+            ascii_string += self.bin_contents[starting_addr + idx]
+            idx += 1
+        return ascii_string
+
+    def _rva_to_file_offset(self, rva):
+        file_offs = -1
+        for section_info in self.section_headers_info:
+            if self._rva_belongs_to_section(rva, section_info):
+                file_offs = rva - section_info['Virtual Address'] + section_info['Pointer to Raw Data']
+                break
+        return file_offs
+
+    @staticmethod
+    def _rva_belongs_to_section(rva, section_info):
+        return section_info['Virtual Address'] <= rva < section_info['Virtual Address'] + section_info['Virtual Size']
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    if len(sys.argv) == 2:
+        parser = ParsePE32()
+        parser.execute(sys.argv[1])
+    else:
+        logging.error('Usage: {} <file_path>'.format(sys.argv[0]))
---
+++ pe/32/libs/pe_section_utils.py
@@ -0,0 +1,104 @@
+import struct
+from collections import OrderedDict
+
+
+class PESectionUtils(object):
+
+    def __init__(self, bin_contents):
+        self.bin_contents = bin_contents
+        self.number_of_sections = None
+        self.image_dos_header_offs = 0
+        self.file_header_offs = None
+        self.pe_header_offs = None
+        self.optional_header_offs = None
+
+    def get_sections_info(self):
+        section_headers_info = []
+        optional_header_size = self.get_file_header_optional_header_size()
+        num_of_sections = self.get_fileheader_num_of_sections()
+        section_headers_offs = self.get_optional_header_offs() + optional_header_size
+        for section_header_num in range(num_of_sections):
+            section_headers_info.append(self.get_section_info(section_headers_offs, section_header_num))
+        return section_headers_info
+
+    def get_fileheader_num_of_sections(self):
+        if self.number_of_sections:
+            return self.number_of_sections
+        self.file_header_offs = self.get_file_header_offs()
+        num_of_sections_offs = 0x2
+        # format string: <: little endian. H: 2 bytes (1 word)
+        num_of_sections_value = struct.unpack_from('<H', self.bin_contents, self.file_header_offs + num_of_sections_offs)
+        return num_of_sections_value[0]
+
+    def get_file_header_offs(self):
+        if self.file_header_offs:
+            return self.file_header_offs
+        return self.get_pe_header_offs() + 0x4
+
+    def get_pe_header_offs(self):
+        if self.pe_header_offs:
+            return self.pe_header_offs
+        e_lfanew_offs = 0x3C
+        # format string: <: little endian. L: 4 bytes (1 dword)
+        pe_header_offs = struct.unpack_from('<L', self.bin_contents, self.image_dos_header_offs + e_lfanew_offs)
+        self.pe_header_offs = pe_header_offs[0]
+        return self.pe_header_offs
+
+    def get_file_header_optional_header_size(self):
+        self.file_header_offs = self.get_file_header_offs()
+        optional_header_size_offs = 0x10
+        # format string: <: little endian. H: 2 bytes (1 word)
+        size_of_optional_header = struct.unpack_from('<H', self.bin_contents, self.file_header_offs + optional_header_size_offs)
+        return size_of_optional_header[0]
+
+    def get_optional_header_offs(self):
+        if self.optional_header_offs:
+            return self.optional_header_offs
+        self.optional_header_offs = self.get_pe_header_offs() + 0x18
+        return self.optional_header_offs
+
+    def get_section_info(self, section_headers_offs, section_header_num):
+        section_header_info = OrderedDict({})
+        header_size = 0x24 + 4  # +4 comes from sizeof(IMAGE_SECTION_HEADER.Characteristics) = sizeof(1 dword) = 4 bytes
+        section_header_offs = section_headers_offs + header_size * section_header_num
+        section_header_info['Name'] = self.get_section_name(section_header_offs)
+        section_header_info['Virtual Size'] = self.get_section_virtual_size(section_header_offs)
+        section_header_info['Virtual Address'] = self.get_section_virtual_address(section_header_offs)
+        section_header_info['Size of Raw Data'] = self.get_section_size_of_raw_data(section_header_offs)
+        section_header_info['Pointer to Raw Data'] = self.get_section_pointer_to_raw_data(section_header_offs)
+        return section_header_info
+
+    def get_section_name(self, section_header_offs):
+        # format string: >: big endian. Q: 8 bytes (2 dwords)
+        section_name = struct.unpack_from('>Q', self.bin_contents, section_header_offs)
+        section_name = self._bytes_to_string(section_name[0], 8)
+        return section_name
+
+    def get_section_virtual_size(self, section_header_offs):
+        virtual_size_offs = section_header_offs + 0x8
+        # format string: <: little endian. L: 4 bytes (1 dword)
+        virtual_size = struct.unpack_from('<L', self.bin_contents, virtual_size_offs)
+        return virtual_size[0]
+
+    def get_section_virtual_address(self, section_header_offs):
+        virtual_address_offs = section_header_offs + 0xc
+        # format string: <: little endian. L: 4 bytes (1 dword)
+        virtual_size = struct.unpack_from('<L', self.bin_contents, virtual_address_offs)
+        return virtual_size[0]
+
+    def get_section_size_of_raw_data(self, section_header_offs):
+        size_of_raw_data_offs = section_header_offs + 0x10
+        # format string: <: little endian. L: 4 bytes (1 dword)
+        virtual_size = struct.unpack_from('<L', self.bin_contents, size_of_raw_data_offs)
+        return virtual_size[0]
+
+    def get_section_pointer_to_raw_data(self, section_header_offs):
+        pointer_to_raw_data_offs = section_header_offs + 0x14
+        # format string: <: little endian. L: 4 bytes (1 dword)
+        virtual_size = struct.unpack_from('<L', self.bin_contents, pointer_to_raw_data_offs)
+        return virtual_size[0]
+
+    @staticmethod
+    def _bytes_to_string(value, length):
+        # https://gph.is/1KjihQe (https://stackoverflow.com/questions/3673428/convert-int-to-ascii-and-back-in-python)
+        return ''.join(chr((value >> 8 * (length - byte - 1)) & 0xFF) for byte in range(length))
