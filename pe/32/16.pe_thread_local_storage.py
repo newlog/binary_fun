6f0f4bc47ea53580b1e77c3363401473d9329089
Albert Lopez <albert.lopez@attackiq.com>
2017-12-27 15:21:57 -0800

    Add code for parsing TLS for x86

---
+++ pe/32/16.pe_thread_local_storage.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+import logging
+import sys
+from libs.pe_generic_utils import PEGenericUtils
+from libs.pe_sections_utils import PESectionUtils
+from libs.pe_data_directory_utils import PEDataDirectoryUtils
+from libs.pe_optional_header_utils import PEOptionalHeaderUtils
+
+
+class FileUtils(object):
+
+    @staticmethod
+    def read_file(path):
+        file_contents = None
+        try:
+            with open(path, 'rb') as fd:
+                file_contents = fd.read()
+        except IOError as e:
+            logging.error('Error reading file "{}". Error: {}'.format(path, e))
+        return file_contents
+
+
+class ParsePE32(object):
+
+    def __init__(self):
+        self.bin_contents = None
+        self.section_headers_info = None
+        self.data_directory_info = None
+        self.pe_section_utils = None
+        self.pe_data_directory_utils = None
+        self.pe_optional_header_utils = None
+        self.image_tls_directory_foff = None
+
+    def execute(self, bin_path):
+        self.bin_contents = FileUtils.read_file(bin_path)
+        if self.bin_contents:
+            self.parse_pe32()
+
+    def parse_pe32(self):
+        self.pe_section_utils = PESectionUtils(self.bin_contents)
+        self.section_headers_info = self.pe_section_utils.get_sections_info()
+        self.pe_data_directory_utils = PEDataDirectoryUtils(self.bin_contents)
+        self.data_directory_info = self.pe_data_directory_utils.get_data_directories_info()
+        self.pe_optional_header_utils = PEOptionalHeaderUtils(self.bin_contents)
+        self.image_tls_directory_foff = self.get_image_tls_directory_rva()
+        tls_callback_vas = self.get_tls_callbacks()
+        logging.info('TLS Callback addresses({} TLS callbacks):'.format(len(tls_callback_vas)))
+        for callback_va in tls_callback_vas:
+            callback_rva = callback_va - self.pe_optional_header_utils.get_image_base()
+            callback_foff = PEGenericUtils.rva_to_file_offset(self.section_headers_info, callback_rva)
+            logging.info('  VA: 0x{:x}, RVA: 0x{:x}, File Offset: {}'.format(callback_va, callback_rva, callback_foff))
+
+    def get_image_tls_directory_rva(self):
+        if self.image_tls_directory_foff:
+            return self.image_tls_directory_foff
+        tls_rva = self.data_directory_info[9]['rva']
+        return tls_rva
+
+    def get_tls_callbacks(self):
+        tls_callback_vas = []
+        callback_number = 0
+        address_of_callbacks_va = -1
+        while address_of_callbacks_va != 0:
+            address_of_callbacks_va = self.get_address_of_callbacks_va(callback_number)
+            if address_of_callbacks_va:
+                tls_callback_vas.append(address_of_callbacks_va)
+                callback_number += 1
+        return tls_callback_vas
+
+    def get_address_of_callbacks_va(self, callback_number):
+        address_of_callbacks_off = 0xC + callback_number * 4  # sizeof(DWORD)
+        image_tls_directory_rva = self.get_image_tls_directory_rva()
+        image_tls_directory_foff = PEGenericUtils.rva_to_file_offset(self.section_headers_info, image_tls_directory_rva)
+        address_of_callbacks_va = PEGenericUtils.unpack_dword(self.bin_contents, image_tls_directory_foff + address_of_callbacks_off)
+        return address_of_callbacks_va
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    if len(sys.argv) == 2:
+        parser = ParsePE32()
+        parser.execute(sys.argv[1])
+    else:
+        logging.error('Usage: {} <file_path>'.format(sys.argv[0]))
---
+++ pe/32/libs/pe_optional_header_utils.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+import struct
+import logging
+from datetime import datetime
+
+
+class PEOptionalHeaderUtils(object):
+
+    def __init__(self, bin_contents):
+        self.bin_contents = bin_contents
+        self.image_dos_header_offs = 0
+        self.pe_header_offs = self.get_pe_header_offs()
+        self.file_header_offs = self.get_file_header_offs()
+        self.optional_header_offs = self.get_optional_header_offs()
+
+    def get_mz_header_contents(self):
+        e_magic_offs = 0
+        # format string: >: big endian. H: 2 bytes (1 word)
+        mz_header_bytes = struct.unpack_from('>H', self.bin_contents, self.image_dos_header_offs + e_magic_offs)
+        mz_header_string = self._long_to_string(mz_header_bytes[0], 2)
+        return mz_header_string
+
+    def get_pe_header_offs(self):
+        e_lfanew_offs = 0x3C
+        # format string: <: little endian. L: 4 bytes (1 dword)
+        pe_header_offs = struct.unpack_from('<L', self.bin_contents, self.image_dos_header_offs + e_lfanew_offs)
+        return pe_header_offs[0]
+
+    def get_file_header_offs(self):
+        return self.get_pe_header_offs() + 0x4
+
+    def get_fileheader_timedatestamp(self):
+        timedatestamp_offs = 0x4
+        # format string: <: little endian. L: 4 bytes (1 dword)
+        timedatestamp_value = struct.unpack_from('<L', self.bin_contents, self.get_file_header_offs() + timedatestamp_offs)
+        readable_date = self._from_timestamp_to_readable_date(timedatestamp_value[0])
+        return readable_date
+
+    def get_fileheader_machine(self):
+        self.file_header_offs = self.get_file_header_offs()
+        machine_offs = 0x0
+        # format string: <: little endian. H: 2 bytes (1 word)
+        machine_value = struct.unpack_from('<H', self.bin_contents, self.file_header_offs + machine_offs)
+        readable_arch = self._translate_arch(machine_value[0])
+        return readable_arch
+
+    def get_fileheader_characteristics(self):
+        self.file_header_offs = self.get_file_header_offs()
+        characteristics_offs = 0x12
+        # format string: <: little endian. H: 2 bytes (1 word)
+        characteristics_value = struct.unpack_from('<H', self.bin_contents, self.file_header_offs + characteristics_offs)
+        readable_characteristics = self._translate_characteristics(characteristics_value[0])
+        return readable_characteristics
+
+    def get_fileheader_num_of_sections(self):
+        self.file_header_offs = self.get_file_header_offs()
+        num_of_sections_offs = 0x2
+        # format string: <: little endian. H: 2 bytes (1 word)
+        num_of_sections_value = struct.unpack_from('<H', self.bin_contents, self.file_header_offs + num_of_sections_offs)
+        return num_of_sections_value[0]
+
+    def get_optional_header_offs(self):
+        return self.get_pe_header_offs() + 0x18
+
+    def get_optional_header_magic(self):
+        self.optional_header_offs = self.get_optional_header_offs()
+        magic_offs = 0x0
+        # format string: <: little endian. H: 2 bytes (1 word)
+        magic_value = struct.unpack_from('<H', self.bin_contents, self.optional_header_offs + magic_offs)
+        readable_magic = self._translate_optional_header_magic(magic_value[0])
+        return readable_magic
+
+    def get_image_base(self):
+        self.optional_header_offs = self.get_optional_header_offs()
+        image_base_offs = 0x1C
+        # format string: <: little endian. L: 4 bytes (1 dword)
+        image_base_value = struct.unpack_from('<L', self.bin_contents, self.optional_header_offs + image_base_offs)
+        image_base_value = image_base_value[0]
+        return image_base_value
+
+    def get_address_of_entry_point(self):
+        self.optional_header_offs = self.get_optional_header_offs()
+        address_of_entry_point_offs = 0x10
+        # format string: <: little endian. L: 4 bytes (1 dword)
+        address_of_entry_point_value = struct.unpack_from('<L', self.bin_contents, self.optional_header_offs + address_of_entry_point_offs)
+        address_of_entry_point_value = address_of_entry_point_value[0]
+        return address_of_entry_point_value
+
+    def get_security_features_from_dll_characteristics(self):
+        self.optional_header_offs = self.get_optional_header_offs()
+        dll_characteristics_offs = 0x46
+        # format string: <: little endian. L: 4 bytes (1 dword)
+        dll_characteristics_value = struct.unpack_from('<L', self.bin_contents, self.optional_header_offs + dll_characteristics_offs)
+        security_features = self._translate_security_features_from_dll_characteristics(dll_characteristics_value[0])
+        return security_features
+
+    @staticmethod
+    def _long_to_string(value, length):
+        # https://gph.is/1KjihQe (https://stackoverflow.com/questions/3673428/convert-int-to-ascii-and-back-in-python)
+        return ''.join(chr((value >> 8 * (length - byte - 1)) & 0xFF) for byte in range(length))
+
+    @staticmethod
+    def _from_timestamp_to_readable_date(timestamp):
+        readable_date = ''
+        try:
+            utc_time = datetime.utcfromtimestamp(timestamp)
+            readable_date = utc_time.strftime("%Y-%m-%d %H:%M:%S.%f+00:00 (UTC)")
+        except ValueError as e:
+            logging.error('Timestamp "{}" could not be converted to an UTC date. Error: {}'.format(timestamp, e))
+        return readable_date
+
+    @staticmethod
+    def _translate_arch(value):
+        if value == 0x14C:
+            readable_arch = 'x86'
+        elif value == 0x8664:
+            readable_arch = 'x86_64'
+        elif value == 0x200:
+            readable_arch = 'IA64'
+        else:
+            readable_arch = 'Unsupported Architecture'
+        return readable_arch
+
+    @staticmethod
+    def _translate_characteristics(value):
+        is_dll = 'DLL: True' if value & 0x2000 else 'DLL: False'
+        is_32bit = '32-bit: True' if value & 0x0100 else '32-bit: False'
+        is_16bit = '16-bit: True' if value & 0x0040 else '16-bit: False'
+        is_stripped = 'Stripped: True' if value & 0x0200 else 'Stripped: False'
+        readable_characteristics = ', '.join([is_dll, is_32bit, is_16bit, is_stripped])
+        return readable_characteristics
+
+    @staticmethod
+    def _translate_optional_header_magic(value):
+        if value == 0x10B:
+            readable_magic = 'PE32'
+        elif value == 0x20B:
+            readable_magic = 'PE32+'
+        elif value == 0x107:
+            readable_magic = 'ROM'
+        else:
+            readable_magic = 'Unsupported Binary Type'
+        return readable_magic
+
+    @staticmethod
+    def _translate_security_features_from_dll_characteristics(value):
+        has_aslr = 'ASLR: True' if value & 0x40 else 'ASLR: False'
+        has_dep = 'DEP: True' if value & 0x0100 else 'DEP: False'
+        has_seh = 'SafeSEH: False' if value & 0x400 else 'SafeSEH: True'
+        security_features = ', '.join([has_aslr, has_dep, has_seh])
+        return security_features
